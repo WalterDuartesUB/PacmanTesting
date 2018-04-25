6e94dc5bcda133178cb42e1f6943b49c66d3a81a
fernandoromero91 <37422646+fernandoromero91@users.noreply.github.com>
2018-04-25 11:55:29 -0300

    Dibujar el mapa del pacman a partir de un archivo de txt llamado Maze.

--- src/ar/edu/ub/testing/pacman/modelo/maze/Maze.java
+++ src/ar/edu/ub/testing/pacman/modelo/maze/Maze.java
@@ -22,7 +22,6 @@ public class Maze
 {
 	
     private static final String DATA_TXT_DIR = "C:\\Sistemas\\Pacman\\Maze.txt";   // provisorio
-    private static final String DATA_TXT_DIR2 = "C:\\Sistemas\\Pacman\\Maze2.txt";   // provisorio version fea 
     
     
 	class MazeCell {
@@ -91,86 +90,96 @@ public class Maze
 		}
 	}
 
-	public static Maze construirMaze() throws FileNotFoundException, IOException 
-	{
-		// TODO Construyo el laberinto con la especificacion default
+	public static Maze construirMazeDefault() {
 		Maze maze = new Maze();
 		
-		String[] prueba = muestraContenido(DATA_TXT_DIR2);
-		
-		
-		System.out.println(prueba[1]);
-		System.out.println(prueba.length); // Alto
-		System.out.println(prueba[0].length()); // ancho
-		
-		// Tengo que resolver despues el spli de cada linea o volver a pensar desde entrada como esta leyendo el txt
-		
-		
-		// MAPA DEFAULT (Deberia moverse eventualmente a una constante)
+		// MAPA DEFAULT (Deberia moverse eventualmente a una constante
 		String mapaDefault = "WWWWWWWWWWWWWWWWWWWWWWWWWWWW\r\n" +
-							 "W............WW............W\r\n" +
-							 "W.WWWW.WWWWW.WW.WWWWW.WWWW.W\r\n" +
-							 "W*W__W.W___W.WW.W___W.W__W*W\r\n" +
-							 "W.WWWW.WWWWW.WW.WWWWW.WWWW.W\r\n" +
-							 "W..........................W\r\n" +
-							 "W.WWWW.WW.WWWWWWWW.WW.WWWW.W\r\n" +
-							 "W.WWWW.WW.WWWWWWWW.WW.WWWW.W\r\n" +
-							 "W......WW....WW....WW......W\r\n" +
-							 "WWWWWW.WWWWW_WW_WWWWW.WWWWWW\r\n" +
-							 "_____W.WW____________.W_____\r\n" +
-							 "_____W.WW_WWW__WWW_WW.W_____\r\n" +
-							 "_____W.WW_W______W_WW.W_____\r\n" +
-							 "WWWWWW.WW_W______W_WW.WWWWWW\r\n" +
-							 "______.___W__G___W___.______\r\n" +
-							 "WWWWWW.WW_W______W_WW.WWWWWW\r\n" +
-							 "_____W.WW_W______W_WW.W_____\r\n" +
-							 "_____W.WW_WWWWWWWW_WW.W_____\r\n" +
-							 "_____W.WW____P_____WW.W_____\r\n" +
-							 "WWWWWW.WW_WWWWWWWW_WW.WWWWWW\r\n" +
-							 "W............WW............W\r\n" +
-							 "W.WWWW.WWWWW.WW.WWWWW.WWWW.W\r\n" +
-							 "W.WWWW.WWWWW.WW.WWWWW.WWWW.W\r\n" +
-							 "W*..WW....__________....WW*W\r\n" +
-							 "WWW.WW.WW.WWWWWWWWWW.WW.WW.W\r\n" +
-							 "WWW.WW.WW.WWWWWWWWWW.WW.WW.W\r\n" +
-							 "W......WW.....WW.....WW....W\r\n" +
-							 "W.WWWWWWWWWWW.WW.WWWWWWWWW.W\r\n" +
-							 "W.WWWWWWWWWWW.WW.WWWWWWWWW.W\r\n" +
-							 "W..........................W\r\n" +
-							 "WWWWWWWWWWWWWWWWWWWWWWWWWWWW\r\n"; 
+				 "W............WW............W\r\n" +
+				 "W.WWWW.WWWWW.WW.WWWWW.WWWW.W\r\n" +
+				 "W*W__W.W___W.WW.W___W.W__W*W\r\n" +
+				 "W.WWWW.WWWWW.WW.WWWWW.WWWW.W\r\n" +
+				 "W..........................W\r\n" +
+				 "W.WWWW.WW.WWWWWWWW.WW.WWWW.W\r\n" +
+				 "W.WWWW.WW.WWWWWWWW.WW.WWWW.W\r\n" +
+				 "W......WW....WW....WW......W\r\n" +
+				 "WWWWWW.WWWWW_WW_WWWWW.WWWWWW\r\n" +
+				 "_____W.WW____________.W_____\r\n" +
+				 "_____W.WW_WWW__WWW_WW.W_____\r\n" +
+				 "_____W.WW_W______W_WW.W_____\r\n" +
+				 "WWWWWW.WW_W______W_WW.WWWWWW\r\n" +
+				 "______.___W__G___W___.______\r\n" +
+				 "WWWWWW.WW_W______W_WW.WWWWWW\r\n" +
+				 "_____W.WW_W______W_WW.W_____\r\n" +
+				 "_____W.WW_WWWWWWWW_WW.W_____\r\n" +
+				 "_____W.WW____P_____WW.W_____\r\n" +
+				 "WWWWWW.WW_WWWWWWWW_WW.WWWWWW\r\n" +
+				 "W............WW............W\r\n" +
+				 "W.WWWW.WWWWW.WW.WWWWW.WWWW.W\r\n" +
+				 "W.WWWW.WWWWW.WW.WWWWW.WWWW.W\r\n" +
+				 "W*..WW....__________....WW*W\r\n" +
+				 "WWW.WW.WW.WWWWWWWWWW.WW.WW.W\r\n" +
+				 "WWW.WW.WW.WWWWWWWWWW.WW.WW.W\r\n" +
+				 "W......WW.....WW.....WW....W\r\n" +
+				 "W.WWWWWWWWWWW.WW.WWWWWWWWW.W\r\n" +
+				 "W.WWWWWWWWWWW.WW.WWWWWWWWW.W\r\n" +
+				 "W..........................W\r\n" +
+				 "WWWWWWWWWWWWWWWWWWWWWWWWWWWW\r\n"; 
 		
 		maze.loadFromBuffer( mapaDefault );
 		
 		return maze;
 	}
 	
+	public static Maze construirMaze() throws FileNotFoundException, IOException 
+	{
+		// TODO Construyo el laberinto con la especificacion default
+		Maze maze = new Maze();
+		String mapaPersonalizado = leerTxt(DATA_TXT_DIR);	
+		maze.loadFromBuffer( mapaPersonalizado);
+		return maze;
+	}
 	
-    public static String[] muestraContenido(String archivo) throws FileNotFoundException, IOException {
-        String cadena;
-        pruebas = null;
-        FileReader f = new FileReader(archivo);
-        BufferedReader b = new BufferedReader(f);
-        while((cadena = b.readLine())!=null) {
-        //    System.out.println(cadena);
-            pruebas = cadena.split("--");    // Esto lo lee si fuera asi wwwwwwwww--w.....w....--etc etc 
-            /*
-             * La idea es que tendria que ser cadena.split("\\r\\n") 
-             * Para leer por ejemplo: 
-             * 
-             * WWWWWWWWWWW
-             * W....W..... 
-             */
-        }
-        b.close();
-        
-        return pruebas;
+    public static String leerTxt(String archivo) throws FileNotFoundException, IOException{
+    	FileReader f = new FileReader(archivo);
+    	BufferedReader b = new BufferedReader(f);
+    	int i = 0;
+    	int cantidadLineas = obtenerCantidadLineas(archivo);
+    	// Falta validar el tamaño del mapa personalizado
+    	 String [] mapaProvisorio = new String[cantidadLineas];
+    	for (String line = b.readLine(); line != null ; line = b.readLine()) {
+    		mapaProvisorio[i] = line;
+    		i++;
+    	}
+    	b.close();
+    	String mapa = pasamanoProvisorio(mapaProvisorio);
+    	return mapa;
     }
-
+    
+	public static int obtenerCantidadLineas(String archivo) throws IOException {
+		
+		FileReader f = new FileReader(archivo);
+		BufferedReader b = new BufferedReader(f);
+		int cantidadLineas = 0;
+		for (String line = b.readLine(); line != null ; line = b.readLine()) {
+			cantidadLineas ++ ;
+		}
+		b.close();	
+		return cantidadLineas;
+	}
+	
+	public static String pasamanoProvisorio(String [] prueba) {
+		String mapa = "";
+		for (int i = 0; i <prueba.length ; i++) {
+			mapa = mapa +prueba[i] +"\r\n";
+		}
+		return mapa;
+	}
+    
 	/**
 	 * Dado un string, crea un mapa
 	 * @param buffer un String con el mapa para jugar
 	 */
-
 	private void loadFromBuffer(String buffer)
 	{
 
@@ -234,7 +243,10 @@ public class Maze
 		// TODO Si no puede crear el laberinto por formato invalido, debe dar una excepcion
 		return null;
 	}
-
+	
+	public static Maze construirMazeDefault(String pendienteMenu) {
+		return null;
+	}
 
 	public Ghost[] getFantasmas()
 	{
